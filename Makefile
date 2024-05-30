659ab12e80e8e3e41284c0a1832c560ba625b867
David Lynch <davite3@protonmail.com>
2024-05-30 15:10:09 +0100

    simple nginx server

--- Makefile
+++ Makefile
@@ -1,14 +1,16 @@
-k8s: k8s-master.nix
-	nix-build '<nixpkgs/nixos>' -A vm -I nixos-config=./k8s-master.nix
-
 vm: machine-config.nix
-	nix-build '<nixpkgs/nixos>' -A vm -I nixos-config=./machine-config.nix
+	nix-build '<nixpkgs/nixos>'\
+		-A vm -I nixos-config=./machine-config.nix
 
 run: vm
 	./result/bin/run-nixos-vm
 
-k8s-run: k8s
+k8s: k8s-master.nix
+	nix-build '<nixpkgs/nixos>' \
+		-A vm -I nixos-config=./k8s-master.nix
+
+run-k8s: k8s
 	./result/bin/run-kooberneets-vm
 
 clean:
-	rm -rf ./nixos.qcow2 ./result/
+	rm -rf *.qcow2 ./result/
--- machine-config.nix
+++ machine-config.nix
@@ -21,9 +21,27 @@ with lib; {
     boot.loader.timeout = 0;
     boot.kernelPackages = pkgs.linuxPackages_latest-libre;
 
-    virtualisation.memorySize = 4096;
-    virtualisation.cores = 2;
-    virtualisation.graphics = false;
+    virtualisation = {
+      memorySize = 4096;
+      cores = 2;
+      graphics = false;
+    };
+
+    services = {
+      nginx = {
+        enable = true;
+        virtualHosts."test.org" = {
+          addSSL = true;
+          enableACME = true;
+          root = "/var/www/test.org";
+        };
+      };
+    };
+
+    security.acme = {
+      acceptTerms = true;
+      defaults.email = "hello@world.com";
+    };
 
     users.extraUsers.root.password = "a";
 
